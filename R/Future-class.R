6a0de5e9979680b50237ded9b202894078aa6a15
hb <hb@aroma-project.org>
2015-06-18 18:45:00 -0700

    Sanity check

--- R/Future-class.R
+++ R/Future-class.R
@@ -74,6 +74,10 @@ Future <- function(expr=NULL, envir=parent.frame(), substitute=FALSE, ...) {
 value.Future <- function(future, onError=c("signal", "return"), ...) {
   onError <- match.arg(onError)
 
+  if (!future$state %in% c('finished', 'failed', 'interrupted')) {
+    stop("Internal error: value() called on a non-finished future: ", class(future)[1])
+  }
+
   value <- future$value
   if (future$state == 'failed' && onError == "signal") stop(value)
 
