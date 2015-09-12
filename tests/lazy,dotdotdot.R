d04ce9de81a5d1f96191344321695272a6bcac97
hb <hb@aroma-project.org>
2015-09-11 19:43:33 -0700

    Add test for `...` in future expressions

---
+++ tests/lazy,dotdotdot.R
@@ -0,0 +1,49 @@
+library("future")
+library("listenv")
+
+ovars <- ls()
+oopts <- options(warn=1L)
+
+
+sum_fcns <- list()
+
+sum_fcns[[1]] <- function(x, ...) {
+  message("Arguments '...' exists: ", exists("...", inherits=TRUE))
+  y %<=% { sum(x, ...) }
+  y
+}
+
+
+sum_fcns[[2]] <- function(x, y) {
+  message("Arguments '...' exists: ", exists("...", inherits=TRUE))
+  y %<=% { sum(x, y) }
+  y
+}
+
+sum_fcns[[3]] <- function(x, ...) {
+  sumt <- function(x) {
+    message("Arguments '...' exists: ", exists("...", inherits=TRUE))
+    y %<=% { sum(x, ...) }
+    y
+  }
+  sumt(x)
+}
+
+
+for (strategy in c("eager", "lazy", "multicore")) {
+  plan(strategy, substitute=FALSE)
+
+  for (ii in seq_along(sum_fcns)) {
+    message(sprintf("** Sum function #%d with plan('%s') ...", ii, strategy))
+    sum_fcn <- sum_fcns[[ii]]
+    print(sum_fcn)
+    y <- sum_fcn(1:2,3)
+    print(y)
+    stopifnot(y == 6)
+  }
+}
+
+
+plan(eager)
+options(oopts)
+rm(list=setdiff(ls(), ovars))
