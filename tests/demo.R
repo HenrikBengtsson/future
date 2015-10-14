fe1a536d443d961466402f5c3bb1ff3c4eb18787
hb <hb@aroma-project.org>
2015-10-14 12:11:32 -0700

    TESTS: Test also demo('mandelbrot') for different plans.

---
+++ tests/demo.R
@@ -0,0 +1,23 @@
+library(future)
+
+ovars <- ls()
+oopts <- options(warn=1L)
+
+message("*** Demos ...")
+
+message("*** Mandelbrot demo of the 'future' package ...")
+
+strategies <- c("eager", "lazy")
+if (supportsMulticore()) strategies <- c(strategies, "multicore")
+
+for (strategy in strategies) {
+  message(sprintf("- plan('%s') ...", strategy))
+  plan(strategy)
+  demo("mandelbrot", package="future", ask=FALSE)
+}
+
+message("*** Demos ... DONE")
+
+plan(eager)
+options(oopts)
+rm(list=setdiff(ls(), ovars))
