d981b0808e3b57dc8e3d4d6967ee66c4f566769a
hb <hb@aroma-project.org>
2015-12-19 14:10:47 -0800

    TESTS: Never tested futureOf() with plain environments

--- tests/futureOf_with_environment.R
+++ tests/futureOf_with_environment.R
@@ -1,5 +1,5 @@
 library("future")
-library("listenv")
+
 mstr <- function(...) message(paste(capture.output(str(...)), collapse="\n"))
 
 suppressWarnings(rm(list=c("x", "z")))
@@ -10,37 +10,29 @@ plan(lazy)
 
 message("*** futureOf() with environment ...")
 
-x <- listenv()
+x <- new.env()
 x$a %<=% { 1 }
 
 f1 <- futureOf("a", envir=x)
 print(f1)
 f2 <- futureOf(a, envir=x)
-f3 <- futureOf(1, envir=x)
 f4 <- futureOf(x[["a"]])
 f5 <- futureOf(x$a)
-f6 <- futureOf(x[[1]])
-stopifnot(identical(f2, f1), identical(f3, f2), identical(f4, f3),
-          identical(f5, f4), identical(f6, f5))
+stopifnot(identical(f2, f1), identical(f4, f1), identical(f5, f1))
 
 fs <- futureOf(envir=x)
 print(fs)
 
-## Out-of-bound subscript, cf. lists
-stopifnot(is.na(futureOf(x[[0]], mustExist=FALSE)))
+## Invalid subset
+res <- try(futureOf(x[[0]], mustExist=FALSE), silent=TRUE)
+stopifnot(inherits(res, "try-error"))
+
 res <- try(futureOf(x[[0]], mustExist=TRUE), silent=TRUE)
 stopifnot(inherits(res, "try-error"))
 
-## Out-of-bound subscript, cf lists
-stopifnot(is.na(futureOf(x[[10]], mustExist=FALSE)))
 res <- try(futureOf(x[[10]], mustExist=TRUE), silent=TRUE)
 stopifnot(inherits(res, "try-error"))
 
-## Non-existing object
-res <- try(futureOf(z[[1]], mustExist=TRUE), silent=TRUE)
-stopifnot(inherits(res, "try-error"))
-
-## Invalid subscript
 res <- try(futureOf(x[[1+2i]], mustExist=TRUE), silent=TRUE)
 stopifnot(inherits(res, "try-error"))
 
