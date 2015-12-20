1643d931d035adb23243c25bab32555a09225b3d
hb <hb@aroma-project.org>
2015-12-20 15:47:48 -0800

    Now whichIndex() also accepts data.frames

--- R/utils.R
+++ R/utils.R
@@ -38,16 +38,47 @@ trim <- function(s) {
 
 whichIndex <- function(I, dim, dimnames=NULL) {
   ndim <- length(dim)
-  stopifnot(is.matrix(I), ncol(I) == ndim)
+  stopifnot((is.matrix(I) || is.data.frame(I)), ncol(I) == ndim)
   if (!is.null(dimnames)) stopifnot(length(dimnames) == ndim)
   if (ndim == 0L) return(integer(0L))
 
-  ## By dimnames?
-  if (is.character(I)) {
+  if (is.data.frame(I)) {
+    ## Convert each column to indices
+    I2 <- array(NA_integer_, dim=dim(I))
+    for (kk in 1:ndim) {
+      idxs <- I[[kk]]
+      if (is.numeric(idxs)) {
+        if (any(idxs < 1 | idxs > dim[kk])) {
+          stop("Index out of range.")
+        }
+      } else {
+        ## Could be, say, factor
+        idxs <- as.character(idxs)
+        idxs <- match(idxs, dimnames[[kk]])
+        if (anyNA(idxs)) {
+          unknown <- I[is.na(idxs),kk]
+          stop("Unknown indices: ", hpaste(sQuote(unknown)))
+        }
+      }
+      I2[,kk] <- idxs
+    }
+    I <- I2
+    I2 <- NULL
+  } else if (is.numeric(I)) {
+    for (kk in 1:ndim) {
+      idxs <- I[,kk]
+      if (any(idxs < 1 | idxs > dim[kk])) {
+        stop("Index out of range.")
+      }
+    }
+  } else {
     ## Convert dimnames to dimindices
     I2 <- array(NA_integer_, dim=dim(I))
     for (kk in 1:ndim) {
-      idxs <- match(I[,kk], dimnames[[kk]])
+      ## Could be, say, factor
+      idxs <- I[,kk]
+      idxs <- as.character(idxs)
+      idxs <- match(idxs, dimnames[[kk]])
       if (anyNA(idxs)) {
         unknown <- I[is.na(idxs),kk]
         stop("Unknown indices: ", hpaste(sQuote(unknown)))
@@ -56,12 +87,6 @@ whichIndex <- function(I, dim, dimnames=NULL) {
     }
     I <- I2
     I2 <- NULL
-  } else {
-    for (kk in 1:ndim) {
-      if (any(I[,kk] < 1 | I[,kk] > dim[kk])) {
-        stop("Index out of range.")
-      }
-    }
   }
 
   ## Nothing more to do?
