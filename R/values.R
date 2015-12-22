e398fd0a5211b24406694c09400831afd78230d3
hb <hb@aroma-project.org>
2015-12-21 18:32:32 -0800

    Documented values() [#33]

--- R/values.R
+++ R/values.R
@@ -1,3 +1,17 @@
+#' Gets all values in an object
+#'
+#' Gets all values in an environment, a list, or a list environment
+#' and returns an object of the same class (and dimensions).
+#' All future elements are replaced by their corresponding
+#' \code{value()} values.  For all other elements, the existing
+#' object is kept.
+#'
+#' @param x An environment, a list, or a list environment.
+#' @param ... Not used.
+#'
+#' @return An object of same type as \code{x} and with the same names
+#' and/or dimensions, if set.
+#'
 #' @export
 values <- function(x, ...) UseMethod("values")
 
