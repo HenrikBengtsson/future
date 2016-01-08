ebdae4996406d3b03d4a08a6f57d9648fd2e3616
hb <hb@aroma-project.org>
2016-01-08 14:46:41 -0800

    Add flapply() - internally for now [#21]

---
+++ R/flapply.R
@@ -0,0 +1,19 @@
+#' @importFrom listenv listenv
+fleapply <- function(x, FUN, ...) {
+  stopifnot(is.function(FUN))
+  res <- listenv()
+  for (ii in seq_along(x)) {
+    res[[ii]] %<=% FUN(x[[ii]], ...)
+  }
+  names(res) <- names(x)
+
+  ## Not needed anymore
+  rm(list=c("x", "FUN", "ii"))
+
+  ## Resolve and return as values
+  values(res)
+} # fleapply()
+
+flapply <- function(x, FUN, ...) {
+  as.list(fleapply(x=x, FUN=FUN, ...))
+}
