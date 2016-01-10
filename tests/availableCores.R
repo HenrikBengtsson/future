b24fca50bfc4304a9fc53bd905aeef963fb692d8
hb <hb@aroma-project.org>
2016-01-09 21:26:42 -0800

    TESTS: Now displaying number of cores and sessions when using covr

--- tests/availableCores.R
+++ tests/availableCores.R
@@ -2,6 +2,9 @@ library("future")
 
 message("*** availableCores() ...")
 
+n <- availableCores()
+message(sprintf("n=%d", n))
+
 print(availableCores())
 print(availableCores("PBS"))
 print(availableCores("system"))
--- tests/availableSessions.R
+++ tests/availableSessions.R
@@ -2,6 +2,9 @@ library("future")
 
 message("*** availableSessions() ...")
 
+n <- availableSessions()
+message(sprintf("n=%d", n))
+
 print(availableSessions())
 print(availableSessions("PBS"))
 print(availableSessions("system"))
