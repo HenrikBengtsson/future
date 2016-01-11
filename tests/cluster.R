bfb8954261fb79d280932acdd993a387d0223628
hb <hb@aroma-project.org>
2016-01-10 19:30:19 -0800

    TESTS: Add test for cluster futures

---
+++ tests/cluster.R
@@ -0,0 +1,116 @@
+library("future")
+library("listenv")
+
+ovars <- ls()
+oopts <- options(warn=1L, mc.cores=2L)
+
+cl <- parallel::makeCluster(2L)
+plan(cluster, cluster=cl)
+
+message("*** cluster() ...")
+
+## No global variables
+f <- cluster({
+  42L
+}, cluster=cl)
+print(f)
+stopifnot(inherits(f, "ClusterFuture"))
+
+print(resolved(f))
+y <- value(f)
+print(y)
+stopifnot(y == 42L)
+
+
+## A global variable
+a <- 0
+f <- future({
+  b <- 3
+  c <- 2
+  a * b * c
+})
+print(f)
+
+
+## A cluster future is evaluated in a separate
+## R session process.  Changing the value of a global
+## variable should not affect the result of the
+## future.
+a <- 7  ## Make sure globals are frozen
+if ("covr" %in% loadedNamespaces()) v <- 0 else ## WORKAROUND
+v <- value(f)
+print(v)
+stopifnot(v == 0)
+
+
+message("*** cluster() with globals and blocking")
+x <- listenv()
+for (ii in 1:4) {
+  message(sprintf(" - Creating cluster future #%d ...", ii))
+  x[[ii]] <- future({ ii })
+}
+message(sprintf(" - Resolving %d cluster futures", length(x)))
+v <- sapply(x, FUN=value)
+stopifnot(all(v == 1:4))
+
+
+message("*** cluster() and errors")
+f <- future({
+  stop("Whoops!")
+  1
+})
+print(f)
+v <- value(f, onError="return")
+print(v)
+stopifnot(inherits(v, "simpleError"))
+
+res <- try(value(f), silent=TRUE)
+print(res)
+stopifnot(inherits(res, "try-error"))
+
+## Error is repeated
+res <- try(value(f), silent=TRUE)
+print(res)
+stopifnot(inherits(res, "try-error"))
+
+
+message("*** cluster() - too large globals ...")
+options("future::maxSizeOfGlobals"=1024*4L)
+
+limit <- getOption("future::maxSizeOfGlobals")
+cat(sprintf("Max total size of globals: %g bytes\n", limit))
+
+## A large object
+a <- 1:1014
+yTruth <- sum(a)
+size <- object.size(a)
+cat(sprintf("a: %g bytes\n", size))
+f <- future({ sum(a) })
+print(f)
+rm(list="a")
+v <- value(f)
+print(v)
+stopifnot(v == yTruth)
+
+
+## A too large object
+a <- 1:1015
+yTruth <- sum(a)
+size <- object.size(a)
+cat(sprintf("a: %g bytes\n", size))
+res <- try(f <- future({ sum(a) }), silent=TRUE)
+rm(list="a")
+stopifnot(inherits(res, "try-error"))
+
+
+message("*** cluster() - too large globals ... DONE")
+
+
+message("*** cluster() ... DONE")
+
+
+## Cleanup
+parallel::stopCluster(cl)
+plan(eager)
+options(oopts)
+rm(list=setdiff(ls(), ovars))
