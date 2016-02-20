46705573bbd68a4009c31d8f6c31d4c32c8a9606
hb <hb@aroma-project.org>
2016-02-19 20:42:15 -0800

    TESTS: Test on invalid ownership requires at least two background processes [#54]

--- tests/invalid-owner.R
+++ tests/invalid-owner.R
@@ -1,5 +1,4 @@
 library("future")
-library("listenv")
 
 ovars <- ls()
 oopts <- options(warn=1L, mc.cores=2L, future.globals.resolve=FALSE, future.debug=TRUE)
@@ -12,24 +11,20 @@ usedNodes <- function(future) {
   c(used=length(future:::FutureRegistry(reg, action="list")), total=length(cluster))
 }
 
-## This R process
-uuid <- future:::uuid()
-cat(sprintf("Main R process: %s\n", uuid))
-
+## This test requires at least two background processes
+plan(multisession, maxCores=3L)
 
 message("*** future() - invalid ownership ...")
 
-plan(multisession)
-print(availableCores(which="all"))
-print(availableCores())
-stopifnot(availableCores() > 2L)
-className <- "MultisessionFuture"
+## This R process
+uuid <- future:::uuid()
+cat(sprintf("Main R process: %s\n", uuid))
 
 message("- Asserting ownership ...")
 
 message("Creating future #1:")
 f1 <- future({ future:::uuid() })
-stopifnot(inherits(f1, className))
+stopifnot(inherits(f1, "MultisessionFuture"))
 cat(sprintf("Future #1 session: %d\n", f1$node))
 v1 <- value(f1)
 cat(sprintf("Future #1 R process: %s\n", v1))
@@ -37,7 +32,7 @@ stopifnot(v1 != uuid)
 
 message("Creating future #2:")
 f2 <- future({ future:::uuid() })
-stopifnot(inherits(f2, className))
+stopifnot(inherits(f2, "MultisessionFuture"))
 cat(sprintf("Future #2 session: %d\n", f2$node))
 v2 <- value(f2)
 cat(sprintf("Future #2 R process: %s\n", v2))
@@ -45,7 +40,7 @@ stopifnot(v2 != uuid)
 
 message("Creating future #3:")
 f3 <- future({ f1$owner })
-stopifnot(inherits(f3, className))
+stopifnot(inherits(f3, "MultisessionFuture"))
 cat(sprintf("Future #3 session: %d\n", f3$node))
 v3 <- value(f3)
 cat(sprintf("Future #3 owner: %s\n", v3))
@@ -53,7 +48,7 @@ stopifnot(v3 == uuid)
 
 message("Creating future #4:")
 f4 <- future({ f1$owner })
-stopifnot(inherits(f4, className))
+stopifnot(inherits(f4, "MultisessionFuture"))
 cat(sprintf("Future #4 session: %d\n", f4$node))
 v4 <- value(f4)
 cat(sprintf("Future #4 owner: %s\n", v4))
@@ -61,7 +56,7 @@ stopifnot(v4 == uuid)
 
 message("Creating future #5:")
 f5 <- future({ stopifnot(f1$owner != future:::uuid()); "not-owner" })
-stopifnot(inherits(f5, className))
+stopifnot(inherits(f5, "MultisessionFuture"))
 v5 <- value(f5)
 stopifnot(v5 == "not-owner")
 
