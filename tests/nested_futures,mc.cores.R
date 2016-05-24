852e5ce4d58944f9ab7d62d692cbfb87232c4940
Henrik Bengtsson <hb@aroma-project.org>
2016-05-24 16:08:49 -0700

    Using plan(list(name, name)) [ci skip]

--- tests/nested_futures,mc.cores.R
+++ tests/nested_futures,mc.cores.R
@@ -16,11 +16,9 @@ for (mc in 0:3) {
   message(sprintf("- mc.cores=%d ...", mc))
   options(mc.cores=mc)
 
-  for (strategyName in strategies) {
-    strategy <- get(strategyName, mode="function")
-
-    message(sprintf("plan(list(eager, %s):", strategyName))
-    plan(list(eager, strategy))
+  for (strategy in strategies) {
+    message(sprintf("plan(list('eager', '%s')):", strategy))
+    plan(list('eager', strategy))
     a %<-% {
       b1 %<-% Sys.getpid()
       b2 %<-% Sys.getpid()
@@ -32,8 +30,8 @@ for (mc in 0:3) {
     stopifnot((mc <= 1 && a$pid2 == pid) || (a$pid2 != pid))
     stopifnot((mc <= 1 && a$pid2 == a$pid1) || (a$pid2 != a$pid1))
 
-    message(sprintf("plan(list(%s, eager):", strategyName))
-    plan(list(strategy, eager))
+    message(sprintf("plan(list('%s', 'eager')):", strategy))
+    plan(list(strategy, 'eager'))
     a %<-% {
       b1 %<-% Sys.getpid()
       b2 %<-% Sys.getpid()
@@ -45,10 +43,10 @@ for (mc in 0:3) {
     stopifnot((mc <= 1 && a$pid2 == pid) || (a$pid2 != pid))
     stopifnot(a$pid2 == a$pid1)
 
-    message(sprintf("plan(list(%s, strategy):", strategyName))
+    message(sprintf("plan(list('%s', '%s')):", strategy, strategy))
     plan(list(strategy, strategy))
     a %<-% {
-      b1 %<-% Sys.getpid()
+      b1 %<-% { Sys.sleep(1); Sys.getpid() }
       b2 %<-% Sys.getpid()
       list(pid=Sys.getpid(), pid1=b1, pid2=b2)
     }
@@ -58,7 +56,8 @@ for (mc in 0:3) {
     stopifnot((mc <= 1 && a$pid2 == pid) || (a$pid2 != pid))
     stopifnot(a$pid2 == a$pid1)
 
-    message(sprintf("plan(list(%s=3, %s=3):", strategyName, strategyName))
+    next
+    message(sprintf("plan(list('%s':3, '%s':3)):", strategy, strategy))
     plan(list(tweak(strategy, workers=3), tweak(strategy, workers=3)))
     a %<-% {
       b1 %<-% Sys.getpid()
@@ -74,7 +73,7 @@ for (mc in 0:3) {
     stopifnot(a$pid1 != pid)
     stopifnot(a$pid2 != pid)
     stopifnot(a$pid2 != a$pid1)
-  } ## for (strategyName ...)
+  } ## for (strategy ...)
 
   message(sprintf(" - mc.cores=%d ... DONE", mc))
 } ## for (mc ...)
