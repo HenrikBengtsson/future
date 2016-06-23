a45a2f95f26d6dffeb7922f392814f9ed8e17c15
hb <hb@aroma-project.org>
2016-06-23 13:10:12 -0700

    TESTS: Testing timeout of requestCore()

--- tests/requestCore.R
+++ tests/requestCore.R
@@ -7,6 +7,20 @@ message("*** requestCore() - exceptions ...")
 res <- try(requestCore(function() {}, workers=1L), silent=TRUE)
 stopifnot(inherits(res, "try-error"))
 
+res <- try(requestCore(function() {}, times=0L), silent=TRUE)
+stopifnot(inherits(res, "try-error"))
+
+message("*** requestCore() - exceptions ... DONE")
+
+
+message("*** requestCore() - timeout ...")
+
+plan(multicore, workers=2L)
+a %<-% { Sys.sleep(3); 1 }
+res <- try(requestCore(function() {}, workers=2L, times=1L, delta=0.1), silent=TRUE)
+stopifnot(inherits(res, "try-error"))
+stopifnot(a == 1)
+
 message("*** requestCore() - exceptions ... DONE")
 
 message("*** requestCore() ... DONE")
