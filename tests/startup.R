3d9deab9b60c3454367545af6b441f0927cad0ac
hb <hb@aroma-project.org>
2016-06-23 17:18:08 -0700

    TESTS: Testing .onAttach() with local .future.R file

--- tests/startup.R
+++ tests/startup.R
@@ -2,6 +2,7 @@ source("incl/start.R")
 
 maxCores <- min(2L, availableCores(methods="system"))
 
+
 plan("default")
 strategy0 <- plan()
  
@@ -167,5 +168,34 @@ message("- .onLoad() w/ future.plan='lazy' & -p 1 ... DONE")
 options(future.plan=NULL, future.cmdargs=NULL)
 
 message("*** .onLoad() ... DONE")
+
+
+message("*** .onAttach() ...")
+
+pkgname <- "future"
+
+message("- .onAttach() w/ option future.startup.loadScript ...")
+
+for (value in list(NULL, FALSE, TRUE)) {
+  options(future.startup.loadScript=value)
+  .onAttach(pkgname, pkgname)
+}
+
+message("- .onAttach() w/ option future.startup.loadScript ... DONE")
+
+message("- .onAttach() with ./.future.R ...")
+
+pathname <- ".future.R"
+xyz <- 0L
+cat("xyz <- 42L; cat('ping\n')\n", file=pathname)
+.onAttach(pkgname, pkgname)
+print(xyz)
+stopifnot(is.integer(xyz), xyz >= 0, xyz == 42L)
+file.remove(pathname)
+
+message("- .onAttach() with ./.future.R ... DONE")
+
+message("*** .onAttach() ... DONE")
+
 
 source("incl/end.R")
