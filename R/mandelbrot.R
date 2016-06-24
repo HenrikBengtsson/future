237880bc84d7e582746cb132a7cad51dca7acdd1
hb <hb@aroma-project.org>
2016-06-23 18:05:47 -0700

    Mandelbrot cannot be plotted on R (<= 3.2.0)

--- R/mandelbrot.R
+++ R/mandelbrot.R
@@ -18,7 +18,10 @@
 #'
 #' @examples
 #' counts <- mandelbrot(xmid=-0.75, ymid=0, side=3)
+#' str(counts)
+#' \dontrun{
 #' plot(counts)
+#' }
 #' 
 #' \dontrun{
 #' demo("mandelbrot", package="future", ask=FALSE)
--- tests/mandelbrot.R
+++ tests/mandelbrot.R
@@ -2,8 +2,12 @@ source("incl/start.R")
 
 message("mandelbrot() ...")
 
-img <- mandelbrot(xmid=-0.75, ymid=0.0, side=3.0, resolution=100L)
-plot(img)
+counts <- mandelbrot(xmid=-0.75, ymid=0.0, side=3.0, resolution=100L)
+img <- as.raster(counts)
+if (getRversion() >= "3.2.0") {
+  plot(img)
+  plot(counts)
+}
 
 message("mandelbrot() ... DONE")
 
