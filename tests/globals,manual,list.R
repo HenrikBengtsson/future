f81b8adbffe293fde5730f7722b4ca5dbb807ba0
hb <hb@aroma-project.org>
2016-08-09 15:55:27 +0200

    TESTS: Add tests for specifying globals via a named list [#84]

---
+++ tests/globals,manual,list.R
@@ -0,0 +1,77 @@
+source("incl/start.R")
+library("listenv")
+
+message("*** Globals - manually ...")
+
+message("*** Globals manually specified as named list ...")
+
+globals <- list(
+  a=1,
+  b=2,
+  sumtwo=function(x) x[1] + x[2]
+)
+
+## Assign 'globals' globally
+for (name in names(globals)) {
+  assign(name, value=globals[[name]])
+}
+
+## Truth
+v0 <- local({
+  x <- 1:10
+  sumtwo(a + b*x)
+})
+
+
+message("*** Globals - automatic ...")
+
+for (strategy in supportedStrategies()) {
+  message(sprintf("- Strategy: %s ...", strategy))
+  
+  plan(strategy)
+ 
+  f <- future({
+    x <- 1:10
+    sumtwo(a + b*x)
+  }, globals=TRUE)
+  print(f)
+  
+  v <- value(f)
+  print(v)
+  stopifnot(all.equal(v, v0))
+  
+  message(sprintf("- Strategy: %s ... DONE", strategy))
+}
+
+message("*** Globals - automatic ... DONE")
+
+message("*** Globals manually specified as named list ...")
+
+for (strategy in supportedStrategies()) {
+  message(sprintf("- Strategy: %s ...", strategy))
+  
+  plan(strategy)
+
+  ## Make sure globals do not exist
+  rm(list=names(globals))
+  
+  f <- future({
+    x <- 1:10
+    sumtwo(a + b*x)
+  }, globals=globals)
+  print(f)
+  
+  v <- value(f)
+  print(v)
+  stopifnot(all.equal(v, v0))
+  
+  message(sprintf("- Strategy: %s ... DONE", strategy))
+}
+
+message("*** Globals manually specified as named list ... DONE")
+
+
+message("*** Globals - manually ... DONE")
+
+source("incl/end.R")
+
