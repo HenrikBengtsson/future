283b058711579d6e7e968482f9bfcd440a611949
hb <hb@aroma-project.org>
2016-09-23 12:03:24 -0700

    ROBUSTNESS: Now the default future strategy is explicitly set when no more strategies are available. Previously only mc.cores was set.

--- tests/nested_futures.R
+++ tests/nested_futures.R
@@ -8,59 +8,47 @@ message("*** Nested futures ...")
 for (strategy1 in strategies) {
   for (strategy2 in strategies) {
     message(sprintf("- plan(list('%s', '%s')) ...", strategy1, strategy2))
-    plan(list(a="lazy", b=strategy1, c=strategy2))
+    plan(list(a=strategy1, b=strategy2))
     
     nested <- plan("list")
     stopifnot(
-      length(nested) == 3L,
-      all(names(nested) == c("a", "b", "c")),
-      inherits(plan(), "lazy")
+      length(nested) == 2L,
+      all(names(nested) == c("a", "b")),
+      inherits(plan(), strategy1)
     )
     
     x %<-% {
       a <- 1L
+
+      ## IMPORTANT: Use future::plan() - not just plan() - otherwise
+      ## we're exporting the plan() function including its local stack!
+      plan_a <- future::plan("list")
+      nested_a <- nested[-1]
       
-      plan_a <- plan("list")
-      nested_a <- nested[-1] 
       stopifnot(
-        length(nested_a) == 2L,
-        length(plan_a) == 2L,
+        length(nested_a) == 1L,
+        length(plan_a) == 1L,
 	inherits(plan_a[[1]], "future"),
         all.equal(plan_a, nested_a),
-        inherits(plan(), strategy1)
+        inherits(future::plan(), strategy2)
       )
       
       y %<-% {
         b <- 2L
-        plan_b <- plan("list")
+	
+        ## IMPORTANT: Use future::plan() - not just plan() - otherwise
+        ## we're exporting the plan() function including its local stack!
+        plan_b <- future::plan("list")
         nested_b <- nested_a[-1]
         stopifnot(
-          length(nested_b) == 1L,
+          length(nested_b) == 0L,
           length(plan_b) == 1L,
 	  inherits(plan_b[[1]], "future"),
-          all.equal(plan_b, nested_b),
-          inherits(plan(), strategy2)
+          inherits(future::plan(), getOption("future.default", "eager"))
         )
 
-        z %<-% {
-          c <- 3L
-          plan_c <- plan("list")
-          nested_c <- nested_b[-1]
-          stopifnot(
-            length(nested_c) == 0L,
-            length(plan_c) == 1L,
-  	    inherits(plan_c[[1]], "future"),
-	    ## FIXME:
-  	    ## inherits(plan_c[[1]], "eager"),
-            ## inherits(plan(), "eager"),
-	    TRUE
-          )
-  
-  	  list(a = a, nested_a = nested_a, plan_a = plan_a,
-  	       b = b, nested_b = nested_b, plan_b = plan_b,
-  	       c = c, nested_c = nested_c, plan_c = plan_c)
-	}
-	z
+	list(a = a, nested_a = nested_a, plan_a = plan_a,
+  	     b = b, nested_b = nested_b, plan_b = plan_b)
       }
       y
     }
@@ -68,27 +56,23 @@ for (strategy1 in strategies) {
     str(x)
 
     stopifnot(
-      length(x) == 3*3,
+      length(x) == 3 * length(nested),
       all(names(x) == c("a", "nested_a", "plan_a",
-                        "b", "nested_b", "plan_b",
-			"c", "nested_c", "plan_c")),
+                        "b", "nested_b", "plan_b")),
+			
       x$a == 1L,
-      x$b == 2L,
-      x$c == 3L,
+      length(x$nested_a) == 1L,
       is.list(x$plan_a),
-      length(x$plan_a) == 2L,
+      length(x$plan_a) == 1L,
       inherits(x$plan_a[[1]], "future"),
       all.equal(x$plan_a, nested[-1L]),
+      
+      x$b == 2L,
+      length(x$nested_b) == 0L,
       is.list(x$plan_b),
       length(x$plan_b) == 1L,
       inherits(x$plan_b[[1]], "future"),
-      all.equal(x$plan_b, nested[-(1:2)]),
-      is.list(x$plan_c),
-      length(x$plan_c) == 1L,
-      inherits(x$plan_c[[1]], "future"),
-      ## FIXME:
-      all.equal(x$plan_b, nested[-(1:2)]),
-      TRUE
+      inherits(x$plan_b[[1]], getOption("future.default", "eager"))
     )
 
     rm(list=c("nested", "x"))
