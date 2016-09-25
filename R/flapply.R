6763b9745ba0f4ba52aec2e8d7b8eec43c41bfef
hb <hb@aroma-project.org>
2016-09-24 17:35:07 -0700

    SPEEDUP: Internal prototype flapply() now collects globals once - less overhead. CLEANUP: Drop internal prototype fleapply().

--- R/flapply.R
+++ R/flapply.R
@@ -7,44 +7,30 @@
 #'
 #' @return A list.
 #'
-#' @aliases fleapply
 #' @keywords internal
-## FIXME: Do we need an envir argument here?
 flapply <- function(x, FUN, ...) {
   stopifnot(is.function(FUN))
-  
+
+  ## 1. Collect all globals (once)
+  envir <- environment()
+  x_ii <- NULL
+  globals <- c("FUN", "x_ii", names(list(...)), "...")
+  globals <- globalsByName(globals, envir=envir, mustExist=FALSE)
+
+  ## 2. Apply function with fixed set of globals
   fs <- vector("list", length = length(x))
   names(fs) <- names(x)
   
   for (ii in seq_along(x)) {
     ## Subsetting outside future is more efficient
-    x_ii <- x[[ii]]
-    fs[[ii]] <- future(FUN(x_ii, ...))
+    globals$x_ii <- x[[ii]]
+    
+    fs[[ii]] <- future(FUN(x_ii, ...), envir=envir, globals=globals)
   }
 
   ## Not needed anymore
-  rm(list=c("x", "FUN", "ii"))
+  rm(list=c("x_ii", "globals", "ii", "envir"))
 
   ## Resolve and return as values
   values(fs)
-}
-
-
-## FIXME: Do we need an envir argument here?
-#' @importFrom listenv listenv
-fleapply <- function(x, FUN, ...) {
-  stopifnot(is.function(FUN))
-  fs <- listenv()
-  for (ii in seq_along(x)) {
-    ## Subsetting outside future is more efficient
-    x_ii <- x[[ii]]
-    fs[[ii]] <- future(FUN(x_ii, ...))
-  }
-  names(fs) <- names(x)
-
-  ## Not needed anymore
-  rm(list=c("x", "FUN", "ii"))
-
-  ## Resolve and return as values
-  values(fs)
-} # fleapply()
+} # flapply()
