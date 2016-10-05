656022fdf17ad57824ef652258a9f73357de3794
hb <hb@aroma-project.org>
2016-10-05 11:13:12 -0700

    Add  to set label in future assignments [#96]

--- tests/future,labels.R
+++ tests/future,labels.R
@@ -10,12 +10,6 @@ for (strategy in strategies) {
   plan(strategy)
 
   for (label in list(NULL, sprintf("strategy=%s", strategy))) {
-    f <- future(42, label = label)
-    print(f)
-    stopifnot(identical(f$label, label))
-    v <- value(f)
-    stopifnot(v == 42)
-    
     fcn <- get(strategy, mode="function")
     stopifnot(inherits(fcn, strategy))
     f <- fcn(42, label = label)
@@ -23,6 +17,19 @@ for (strategy in strategies) {
     stopifnot(identical(f$label, label))
     v <- value(f)
     stopifnot(v == 42)
+    
+    f <- future(42, label = label)
+    print(f)
+    stopifnot(identical(f$label, label))
+    v <- value(f)
+    stopifnot(v == 42)
+
+    v %<-% { 42 } %label% label
+    f <- futureOf(v)
+    print(f)
+    stopifnot(identical(f$label, label))
+    stopifnot(v == 42)
+
   } ## for (label ...)
 
   message(sprintf("- plan('%s') ... DONE", strategy))
