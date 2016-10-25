bc0a698633078c1b7f255a99d10df3452df329ee
hb <hb@aroma-project.org>
2016-10-24 20:39:23 -0700

    HELP: Adding makeClusterPSOCK() examples for setting up GCE and AWS EC2 workers

--- incl/makeClusterPSOCK.R
+++ incl/makeClusterPSOCK.R
@@ -4,41 +4,116 @@ cl <- makeClusterPSOCK(workers, dryrun = TRUE)
 
 ## Same setup when the two machines are on the local network and
 ## have identical software setups
-cl <- makeClusterPSOCK(workers, revtunnel = FALSE, homogeneous = TRUE,
-                       dryrun = TRUE)
+cl <- makeClusterPSOCK(
+  workers,
+  revtunnel = FALSE, homogeneous = TRUE,
+  dryrun = TRUE
+)
 
 ## Setup of remote worker with more detailed control on
 ## authentication and reverse SSH tunnelling
-cl <- makeClusterPSOCK("remote.server.org", user = "johnny",
-                       revtunnel = FALSE,
-                       rshopts = c("-v", "-R 11000:gateway:11942"),
-                       master = "gateway", port = 11942,
-                       rscript = c("nice", "/path/to/Rscript"),
-                       rscript_args = c("--vanilla"),
-                       dryrun = TRUE)
+cl <- makeClusterPSOCK(
+  "remote.server.org", user = "johnny",
+  ## Manual configuration of reverse SSH tunnelling
+  revtunnel = FALSE,
+  rshopts = c("-v", "-R 11000:gateway:11942"),
+  master = "gateway", port = 11942,
+  ## Run Rscript nicely and skip any startup scripts
+  rscript = c("nice", "/path/to/Rscript"),
+  rscript_args = c("--vanilla"),
+  dryrun = TRUE
+)
 
 ## Setup of Docker worker running rocker/r-base
 ## (requires installation of future package)
-cl <- makeClusterPSOCK("localhost",
-                       rscript = c(
-                         "docker", "run", "--net=host", "rocker/r-base",
-                         "Rscript"
-                       ),
-                       rscript_args = c(
-                         "-e", shQuote("install.packages('future')")
-                       ),
-                       dryrun = TRUE)
+cl <- makeClusterPSOCK(
+  "localhost",
+  ## Launch Rscript inside Docker container
+  rscript = c(
+    "docker", "run", "--net=host", "rocker/r-base",
+    "Rscript"
+  ),
+  ## Install future package
+  rscript_args = c(
+    "-e", shQuote("install.packages('future')")
+  ),
+  dryrun = TRUE
+)
                        
 
 ## Setup of udocker.py worker running rocker/r-base
 ## (requires installation of future package and extra quoting)
-cl <- makeClusterPSOCK("localhost",
-                       rscript = c(
-                         "udocker.py", "run", "rocker/r-base",
-                         "Rscript"
-                       ),
-                       rscript_args = c(
-                         "-e", shQuote(shQuote("install.packages('future')")),
-                         "-e", shQuote(shQuote("parallel:::.slaveRSOCK()"))
-                       ),
-                       dryrun = TRUE)
+cl <- makeClusterPSOCK(
+  "localhost",
+  ## Launch Rscript inside Docker container (using udocker)
+  rscript = c(
+    "udocker.py", "run", "rocker/r-base",
+    "Rscript"
+  ), 
+  ## Install future package and manually launch parallel workers
+  ## (need double shQuote():s because udocker.py drops one level)
+  rscript_args = c(
+    "-e", shQuote(shQuote("install.packages('future')")),
+    "-e", shQuote(shQuote("parallel:::.slaveRSOCK()"))
+  ),
+  dryrun = TRUE
+)
+
+
+## Launching worker on Amazon AWS EC2 running one of the
+## Amazon Machine Images (AMI) provided by RStudio
+## (http://www.louisaslett.com/RStudio_AMI/)
+public_ip <- "1.2.3.4"
+ssh_private_key_file <- "~/.ssh/my-private-aws-key.pem"
+cl <- makeClusterPSOCK(
+  ## Public IP number of EC2 instance
+  public_ip,
+  ## User name (always 'ubuntu')
+  user = "ubuntu",
+  ## Use private SSH key registered with AWS
+  rshopts=c(
+    "-o", "StrictHostKeyChecking=no",
+    "-o", "IdentitiesOnly=yes",
+    "-i", ssh_private_key_file
+  ),
+  ## Set up .libPaths() for the 'ubuntu' user and
+  ## install future package
+  rscript_args = c(
+    "-e", shQuote("local({
+      p <- Sys.getenv('R_LIBS_USER')
+      dir.create(p, recursive = TRUE, showWarnings = FALSE)
+      .libPaths(p)
+    })"),
+    "-e", shQuote("install.packages('future')")
+  ),
+  dryrun = TRUE
+)
+
+
+## Launching worker on Google Cloud Engine (GCE) running a
+## container based VM (with a #cloud-config specification)
+public_ip <- "1.2.3.4"
+user <- "johnny"
+ssh_private_key_file <- "~/.ssh/google_compute_engine"
+cl <- makeClusterPSOCK(
+  ## Public IP number of GCE instance
+  public_ip,
+  ## User name (== SSH key label (sic!))
+  user = user,
+  ## Use private SSH key registered with GCE
+  rshopts=c(
+    "-o", "StrictHostKeyChecking=no",
+    "-o", "IdentitiesOnly=yes",
+    "-i", ssh_private_key_file
+  ),
+  ## Launch Rscript inside Docker container
+  rscript = c(
+    "docker", "run", "--net=host", "rocker/r-base",
+    "Rscript"
+  ),
+  ## Install future package
+  rscript_args = c(
+    "-e", shQuote("install.packages('future')")
+  ),
+  dryrun = TRUE
+)
