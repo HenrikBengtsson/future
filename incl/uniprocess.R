7305cb318d0056410d4966e0c3e09f755d38f063
hb <hb@aroma-project.org>
2016-11-04 21:11:38 -0700

    HELP: eager() -> uniprocess() [#91]

--- incl/uniprocess.R
+++ incl/uniprocess.R
@@ -1,18 +1,19 @@
-## Use eager futures
-plan(eager)
+## Use uniprocess futures
+plan(uniprocess)
 
 ## A global variable
 a <- 0
 
-## Create eager future (explicitly)
+## Create uniprocess future (explicitly)
 f <- future({
   b <- 3
   c <- 2
   a * b * c
 })
 
-## Since 'a' is a global variable in _eager_ future 'f',
-## it already has been resolved, and any changes to 'a'
+## Since 'a' is a global variable in future 'f' which
+## is eagerly resolved (default), this global has already
+## been resolved / incorporated, and any changes to 'a'
 ## at this point will _not_ affect the value of 'f'.
 a <- 7
 print(a)
