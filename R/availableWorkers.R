6a79c9bd19208879a47b98e829b6d943881da900
hb <hb@aroma-project.org>
2017-01-06 11:17:34 -0800

    Now availableCores() recognizes and parses SGE's PE_HOSTFILE [#118]

--- R/availableWorkers.R
+++ R/availableWorkers.R
@@ -32,6 +32,12 @@
 #'    An example of a job submission that results in this is
 #'    \code{qsub -l nodes=4:ppn=2}, which requests four nodes each
 #'    with two cores.
+#'  \item \code{"SGE"} -
+#'    Query Sun/Oracle Grid Engine (SGE) environment variable
+#'    \env{PE_HOSTFILE}.
+#'    An example of a job submission that results in this is
+#'    \code{qsub -pe mpi 8} (or \code{qsub -pe ompi 8}), which
+#'    requests eight cores on a any number of machines.
 #' }
 #'
 #' @seealso
@@ -45,12 +51,12 @@ availableWorkers <- function(methods=getOption("future.availableCores.methods", 
   ## Local functions
   getenv <- function(name) {
     as.character(trim(Sys.getenv(name, NA_character_)))
-  } # getenv()
+  }
 
   getopt <- function(name) {
     as.character(getOption(name, NA_character_))
-  } # getopt()
-
+  }
+ 
   split <- function(s) {
     x <- unlist(strsplit(s, split = "[,]", fixed = FALSE), use.names = FALSE)
     x <- trim(x)
@@ -81,8 +87,17 @@ availableWorkers <- function(methods=getOption("future.availableCores.methods", 
         warning(sprintf("Environent variable %s was set but no such file %s exists", sQuote("PBS_NODEFILE"), sQuote(pathname)))
 	next
       }
-      ## One node per line
-      w <- readLines(pathname)
+      data <- read_pbs_nodefile(pathname)
+      w <- data$node
+    } else if (method == "SGE") {
+      pathname <- getenv("PE_HOSTFILE")
+      if (is.na(pathname)) next
+      if (!file_test("-f", pathname)) {
+        warning(sprintf("Environent variable %s was set but no such file %s exists", sQuote("PE_HOSTFILE"), sQuote(pathname)))
+	next
+      }
+      data <- read_pe_hostfile(pathname)
+      w <- expand_nodes(data)
     } else {
       ## Fall back to querying option and system environment variable
       ## with the given name
@@ -139,3 +154,67 @@ availableWorkers <- function(methods=getOption("future.availableCores.methods", 
   workers
 } # availableWorkers()
 
+
+
+#' @importFrom utils read.table
+read_pbs_nodefile <- function(pathname, sort = TRUE) {
+  ## One (node) per line
+  lines <- readLines(pathname)
+  lines <- trim(lines)
+
+  ## Sanity checks
+  stopifnot(
+    all(nzchar(lines)),
+    !anyNA(lines),
+    !any(grepl("[[:space:]]", lines))
+  )
+
+  data <- data.frame(node = lines, stringsAsFactors = FALSE)
+
+  if (sort) {
+    data <- data[order(data$node), , drop = FALSE]
+  }
+
+  data
+}
+
+
+#' @importFrom utils read.table
+read_pe_hostfile <- function(pathname, sort = TRUE) {
+  ## One (node, ncores, queue, comment) per line, e.g.
+  ## opt88 3 short.q@opt88 UNDEFINED
+  ## iq242 2 short.q@iq242 UNDEFINED
+  ## opt116 1 short.q@opt116 UNDEFINED
+  data <- read.table(pathname, header = FALSE, sep = " ", stringsAsFactors = FALSE)
+  
+  ## Sanity checks
+  stopifnot(ncol(data) >= 2)
+  
+  colnames(data)[1:2] <- c("node", "count")
+  if (ncol(data) >= 3) colnames(data)[3] <- "via"
+  if (ncol(data) >= 4) colnames(data)[4] <- "notes"
+
+  stopifnot(
+    is.character(data$node),
+    !anyNA(data$node),
+    !any(grepl("[[:space:]]", data$node)),
+    is.integer(data$count),
+    !anyNA(data$count),
+    all(is.finite(data$count)),
+    all(data$count > 0)
+  )
+
+  if (sort) {
+    data <- data[order(data$node, data$count), , drop = FALSE]
+  }
+  
+  data
+}
+
+
+expand_nodes <- function(data) {
+  nodes <- mapply(data$node, data$count, FUN = function(node, count) {
+    rep(node, times = count)
+  }, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+  unlist(nodes, recursive = FALSE, use.names = FALSE)
+}
--- tests/availableWorkers.R
+++ tests/availableWorkers.R
@@ -26,14 +26,84 @@ print(availableWorkers(methods="PBS"))
 print(availableWorkers(methods="SGE"))
 print(availableWorkers(methods="Slurm"))
 
-## PBS settings
+
+
+message("*** HPC related ...")
+
+expand_nodes <- future:::expand_nodes
+read_pbs_nodefile <- future:::read_pbs_nodefile
+read_pe_hostfile <- future:::read_pe_hostfile
+
+workers0 <- c("n1", "n2", "n3", "n1", "n6", "n3", "n3")
+data0 <- as.data.frame(table(workers0), stringsAsFactors = FALSE)
+colnames(data0) <- c("node", "count")
+data0 <- data0[order(data0$node, data0$count), ]
+
+message("*** read_pbs_nodefile() ...")
+
+workers <- workers0
 pathname <- tempfile()
-workers0 <- c("n1", "n2", "n3", "n1", "n6")
-writeLines(workers0, con = pathname)
+writeLines(workers, con = pathname)
+
+data <- read_pbs_nodefile(pathname)
+str(data)
+stopifnot(
+  c("node") %in% colnames(data),
+  is.character(data$node),
+  !anyNA(data$node),
+  nrow(data$node) == length(workers),
+  all(sort(data$node) == sort(workers))
+)
+
 Sys.setenv(PBS_NODEFILE = pathname)
-workers <- availableWorkers(methods="PBS")
+workers <- availableWorkers(methods = "PBS")
 print(workers)
-stopifnot(length(workers) == length(workers0), all(workers == workers0))
+stopifnot(length(workers) == length(workers0), all(workers == sort(workers0)))
+
+
+## Exceptions
+workersE <- c(workers, "n 3")
+pathname <- tempfile()
+writeLines(workersE, con = pathname)
+res <- tryCatch(read_pbs_nodefile(pathname), error = identity)
+print(res)
+stopifnot(inherits(res, "error"))
+
+message("*** read_pbs_nodefile() ... DONE")
+
+
+message("*** read_pe_hostfile() ...")
+
+workers <- workers0
+pathname <- tempfile()
+write.table(data0, file = pathname, quote = FALSE, row.names = FALSE, col.names = FALSE)
+lines <- readLines(pathname)
+print(lines)
+data <- read_pe_hostfile(pathname)
+print(data)
+stopifnot(
+  is.character(data$node),
+  !anyNA(data$node),
+  is.integer(data$count),
+  !anyNA(data$count),
+  all(is.finite(data$count)),
+  all(data$count > 0),
+  nrow(data) == nrow(data0),
+  all.equal(data[,c("node", "count")], data0[,c("node", "count")])
+)
+
+workers <- expand_nodes(data)
+stopifnot(length(workers) == length(workers0), all(workers == sort(workers0)))
+
+Sys.setenv(PE_HOSTFILE = pathname)
+workers <- availableWorkers(methods = "SGE")
+print(workers)
+stopifnot(length(workers) == length(workers0), all(workers == sort(workers0)))
+
+message("*** read_pe_hostfile() ... DONE")
+
+
+message("*** HPC related ... DONE")
 
 
 ## Any R options and system environment variable
@@ -44,7 +114,6 @@ print(availableWorkers(methods=c("width", "FOO_BAR_ENV"),
 Sys.setenv("FOO_BAR_ENV"="0")
 res <- try(availableWorkers(methods="FOO_BAR_ENV"), silent=TRUE)
 stopifnot(inherits(res, "try-error"))
-
 
 message("*** availableWorkers() ... DONE")
 
