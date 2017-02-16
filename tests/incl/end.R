b846ea0981d518158d4f6ae84afb71278b80ec7f
hb <hb@aroma-project.org>
2017-02-15 21:38:04 -0800

    TESTS: Now tests should work also with HPC Denvironment variables which otherwise might conflict with the test results [#124]

--- tests/incl/end.R
+++ tests/incl/end.R
@@ -17,19 +17,22 @@ stopifnot(identical(options(), oopts0))
 ## Undo system environment variables
 ## (a) Added
 cenvs <- Sys.getenv()
-added <- setdiff(names(cenvs), names(oenvs))
+added <- setdiff(names(cenvs), names(oenvs0))
 for (name in added) Sys.unsetenv(name)
-## (b) Modified?
-for (name in intersect(names(cenvs), names(oenvs))) {
+## (b) Missing
+missing <- setdiff(names(oenvs0), names(cenvs))
+if (length(missing) > 0) do.call(Sys.setenv, as.list(oenvs0[missing]))
+## (c) Modified?
+for (name in intersect(names(cenvs), names(oenvs0))) {
   ## WORKAROUND: On Linux Wine, base::Sys.getenv() may
   ## return elements with empty names. /HB 2016-10-06
   if (nchar(name) == 0) next
-  if (!identical(cenvs[[name]], oenvs[[name]])) {
-    do.call(Sys.setenv, as.list(oenvs[name]))
+  if (!identical(cenvs[[name]], oenvs0[[name]])) {
+    do.call(Sys.setenv, as.list(oenvs0[name]))
   }
 }
-## (c) Assert that everything was undone
-stopifnot(identical(Sys.getenv(), oenvs))
+## (d) Assert that everything was undone
+stopifnot(identical(Sys.getenv(), oenvs0))
 
 
 ## Undo variables
