fc87235cd451728b49efed51c58e2461da680e35
hb <hb@aroma-project.org>
2017-04-05 20:42:32 -0700

    TESTS: Added package tests for globals part of formulas part of other globals, e.g. purrr::map(x, ~ rnorm(.)), which requires globals (> 0.9.0) (Issue #137)

--- tests/globals,formulas.R
+++ tests/globals,formulas.R
@@ -11,9 +11,9 @@ message("*** Globals - formulas ...")
 message("*** Globals - lm(<formula>) ...")
 
 ## From example("lm", package="stats")
-ctl <- c(4.17,5.58,5.18,6.11,4.50,4.61,5.17,4.53,5.33,5.14)
-trt <- c(4.81,4.17,4.41,3.59,5.87,3.83,6.03,4.89,4.32,4.69)
-group <- gl(2, 10, 20, labels = c("Ctl","Trt"))
+ctl <- c(4.17, 5.58, 5.18, 6.11, 4.50, 4.61, 5.17, 4.53, 5.33, 5.14)
+trt <- c(4.81, 4.17, 4.41, 3.59, 5.87, 3.83, 6.03, 4.89, 4.32, 4.69)
+group <- gl(2, 10, 20, labels = c("Ctl", "Trt"))
 weight <- c(ctl, trt)
 
 ## Truth:
@@ -22,7 +22,7 @@ print(fit0)
 
 for (cores in 1:min(3L, availableCores())) {
   message(sprintf("Testing with %d cores ...", cores))
-  options(mc.cores=cores-1L)
+  options(mc.cores = cores - 1L)
 
   message("availableCores(): ", availableCores())
 
@@ -74,7 +74,7 @@ print(tbl0)
 
 for (cores in 1:min(3L, availableCores())) {
   message(sprintf("Testing with %d cores ...", cores))
-  options(mc.cores=cores-1L)
+  options(mc.cores = cores - 1L)
 
   message("availableCores(): ", availableCores())
 
@@ -100,32 +100,33 @@ for (cores in 1:min(3L, availableCores())) {
 message("*** Globals - one-side formulas, e.g. xtabs(~ x) ... DONE")
 
 
-message("*** Globals - lm(<formula>, data=cars) ...")
+message("*** Globals - lm(<formula>, data = cars) ...")
 
 exprs <- list(
   # "remove-intercept-term" form of no-intercept
-  a = substitute({ lm(dist ~ . -1, data=cars) }),
+  a = substitute({ lm(dist ~ . -1, data = cars) }),
   # "make-intercept-zero" form of no-intercept
-  b = substitute({ lm(dist ~ . +0, data=cars) }),
+  b = substitute({ lm(dist ~ . +0, data = cars) }),
   # doesn't do what we want here
-  c = substitute({ lm(dist ~ speed + speed^2, data=cars) }),
+  c = substitute({ lm(dist ~ speed + speed ^ 2, data = cars) }),
   # gets us a quadratic term
-  d = substitute({ lm(dist ~ speed + I(speed^2), data=cars) }),
+  d = substitute({ lm(dist ~ speed + I(speed ^ 2), data = cars) }),
   # avoid potential multicollinearity
-  e = substitute({ lm(dist ~ poly(speed,2), data=cars) })
+  e = substitute({ lm(dist ~ poly(speed, 2), data = cars) })
 )
 
 for (kk in seq_along(exprs)) {
   expr <- exprs[[kk]]
   name <- names(exprs)[kk]
-  message(sprintf("- Globals - lm(<formula #%d (%s)>, data=cars) ...", kk, sQuote(name)))
+  message(sprintf("- Globals - lm(<formula #%d (%s)>, data = cars) ...",
+                  kk, sQuote(name)))
 
   fit0 <- eval(expr)
   print(fit0)
 
   for (cores in 1:min(3L, availableCores())) {
     message(sprintf("Testing with %d cores ...", cores))
-    options(mc.cores=cores-1L)
+    options(mc.cores = cores - 1L)
   
     message("availableCores(): ", availableCores())
   
@@ -144,7 +145,59 @@ for (kk in seq_along(exprs)) {
   } ## for (cores ...)
 } ## for (kk ...)
 
-message("*** Globals - lm(<formula>, data=cars) ... DONE")
+message("*** Globals - lm(<formula>, data = cars) ... DONE")
+
+
+message("*** Globals - map(x, ~ expr) ...")
+
+## A fake purrr::map() function with limited functionality
+map <- function(.x, .f, ...) {
+  if (inherits(.f, "formula")) {
+    expr <- .f[[-1]]
+    .f <- eval(bquote(function(...) {
+      .(expr)
+    }))
+  }
+  eval(lapply(.x, FUN = .f, ...))
+}
+
+inner_function <- function(x) { x + 1 }
+
+outer_function <- function(x) {
+  map(1:2, ~ inner_function(.x))
+}
+
+y0 <- outer_function(1L)
+str(y0)
+
+## Check if globals version installed identifies globals in formulas
+formulas_supported <- ("x" %in% globals::findGlobals(~ x, substitute = TRUE))
+if (formulas_supported) {
+  for (cores in 1:min(3L, availableCores())) {
+    message(sprintf("Testing with %d cores ...", cores))
+    options(mc.cores = cores - 1L)
+  
+    message("availableCores(): ", availableCores())
+  
+    for (strategy in strategies) {
+      message(sprintf("- plan('%s') ...", strategy))
+      plan(strategy)
+      
+      f <- future({ outer_function(1L) })
+      y <- value(f)
+      str(y)
+      stopifnot(all.equal(y, y0))
+
+      y %<-% { outer_function(1L) }
+      str(y)
+      stopifnot(all.equal(y, y0))
+    } ## for (strategy ...)
+    
+    message(sprintf("Testing with %d cores ... DONE", cores))
+  } ## for (cores ...)
+}
+
+message("*** Globals - map(x, ~ expr) ... DONE")
 
 
 message("*** Globals - formulas ... DONE")
