305af690e0578f397f903acf9d8ea71a401083ce
hb <hb@aroma-project.org>
2017-05-20 13:10:13 -0700

    TESTS: Add explicit tests for FutureGlobals and makeClusterPSOCK()

---
+++ tests/FutureGlobals.R
@@ -0,0 +1,44 @@
+source("incl/start.R")
+
+## FIXME: Should shallow_resolve() be dropped?
+shallow_resolve <- future:::shallow_resolve.FutureGlobals
+
+message("*** FutureGlobals() ...")
+
+fg1 <- FutureGlobals()
+print(fg1)
+
+fg2 <- FutureGlobals(fg1)
+print(fg2)
+
+g <- globals::as.Globals(list(a = 1, b = 1:3))
+print(g)
+
+fg3 <- FutureGlobals(g)
+print(fg3)
+
+fg4 <- as.FutureGlobals(g)
+print(fg4)
+
+print(resolved(fg3))
+
+fg <- fg4
+
+fg_unique <- unique(fg)
+print(fg_unique)
+
+fg_resolved <- resolve(fg)
+print(fg_resolved)
+
+fg_resolved_s <- shallow_resolve(fg)
+print(fg_resolved_s)
+
+message("- FutureGlobals() - exceptions ...")
+
+res <- tryCatch(g <- FutureGlobals(NULL), error = identity)
+print(res)
+stopifnot(inherits(res, "error"))
+
+message("*** FutureGlobals() ... DONE")
+
+source("incl/end.R")
