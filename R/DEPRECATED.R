b5914b74d8c4f77abce1fc6860c3cd2bcd8927fa
hb <hb@aroma-project.org>
2017-07-04 17:01:07 -0700

    TESTS: Asserting that eager/lazy futures are defunct [#109]

--- R/DEPRECATED.R
+++ R/DEPRECATED.R
@@ -4,7 +4,7 @@ eager <- function(expr, envir = parent.frame(), substitute = TRUE, lazy = FALSE,
   if (substitute) expr <- substitute(expr)
   local <- as.logical(local)
 
-  .Defunct(msg = "Future strategy 'eager' is deprecated. Instead, use 'sequential', which works identically.")
+  .Defunct(msg = "Future strategy 'eager' is defunct. Instead, use 'sequential', which works identically.")
 
   future <- EagerFuture(expr = expr, envir = envir, substitute = FALSE, lazy = lazy, seed = seed, globals = globals, local = local, earlySignal = earlySignal, label = label, ...)
   if (!future$lazy) future <- run(future)
@@ -18,7 +18,7 @@ lazy <- function(expr, envir = parent.frame(), substitute = TRUE, lazy = TRUE, s
   if (substitute) expr <- substitute(expr)
   local <- as.logical(local)
 
-  .Defunct(msg = "Future strategy 'lazy' is deprecated. Instead, use f <- future(..., lazy = TRUE) or v %<-% { ... } %lazy% TRUE.")
+  .Defunct(msg = "Future strategy 'lazy' is defunct. Instead, use f <- future(..., lazy = TRUE) or v %<-% { ... } %lazy% TRUE.")
   
   future <- LazyFuture(expr = expr, envir = envir, local = local, lazy = lazy, seed = seed, globals = globals, earlySignal = earlySignal, label = label, ...)
   if (!future$lazy) future <- run(future)
@@ -37,7 +37,7 @@ class(lazy) <- c("function", class(lazy))
 EagerFuture <- function(expr = NULL, envir = parent.frame(), substitute = FALSE, lazy = FALSE, globals = TRUE, local = TRUE, ...) {
   if (substitute) expr <- substitute(expr)
 
-  .Defunct(msg = "EagerFuture is deprecated. Instead, use SequentialFuture")
+  .Defunct(msg = "EagerFuture is defunct. Instead, use SequentialFuture")
 
   f <- UniprocessFuture(expr = expr, envir = envir, substitute = FALSE, lazy = lazy, globals = globals, local = local, ...)
   structure(f, class = c("EagerFuture", class(f)))
@@ -49,7 +49,7 @@ EagerFuture <- function(expr = NULL, envir = parent.frame(), substitute = FALSE,
 LazyFuture <- function(expr=NULL, envir=parent.frame(), substitute=FALSE, lazy=TRUE, globals=TRUE, local=TRUE, ...) {
   if (substitute) expr <- substitute(expr)
 
-  .Defunct(msg = "LazyFuture is deprecated. Instead, use f <- SequentialFuture(..., lazy = TRUE)")
+  .Defunct(msg = "LazyFuture is defunct. Instead, use f <- SequentialFuture(..., lazy = TRUE)")
 
   f <- UniprocessFuture(expr = expr, envir = envir, substitute = FALSE, lazy = lazy, globals = globals, local = local, ...)
   structure(f, class=c("LazyFuture", class(f)))
