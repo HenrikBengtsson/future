51d12326dff965913f861d82eafdbfd22388a71b
Henrik Bengtsson <hb@aroma-project.org>
2019-01-23 19:37:36 -0800

    TESTS: Output also future:::session_info() - could be useful for troubleshooting [ci skip]

--- tests/000.sessionDetails.R
+++ tests/000.sessionDetails.R
@@ -3,8 +3,16 @@ library("future")
 sd <- sessionDetails()
 print(sd)
 print(sd, output = "message")
-
 rm(list = "sd")
+
+si <- future:::session_info()
+print(si)
+
+si <- future:::session_info(pkgs = TRUE)
+print(si)
+
+rm(list = "si")
+
 
 
 
