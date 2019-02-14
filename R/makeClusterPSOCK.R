3f71bec5710d79ffa345b37d6991089ba89dec8f
Henrik Bengtsson <hb@aroma-project.org>
2019-02-14 12:39:24 -0800

    BUG FIX: makeClusterPSOCK(... rscript) produced warnings when length(rscript) > 1

--- R/makeClusterPSOCK.R
+++ R/makeClusterPSOCK.R
@@ -456,17 +456,22 @@ makeNodePSOCK <- function(worker = "localhost", master = NULL, port, connectTime
     if (autoKill) {
       pidfile <- tempfile(pattern = sprintf("future.parent=%d.", Sys.getpid()), fileext = ".pid")
       pidfile <- normalizePath(pidfile, winslash = "/", mustWork = FALSE)
-      pidcode <- sprintf('try(cat(Sys.getpid(),file="%s"))', pidfile)
+      pidcode <- sprintf('try(suppressWarnings(cat(Sys.getpid(),file="%s")), silent = TRUE)', pidfile)
       rscript_pid_args <- c("-e", shQuote(pidcode))
+      
       ## Check if this approach to infer the PID works
-      test_cmd <- paste(rscript, paste(c(rscript_pid_args, "-e", 42), collapse = " "))
+      test_cmd <- paste(c(rscript, rscript_pid_args, "-e", shQuote(sprintf("file.exists(%s)", shQuote(pidfile)))), collapse = " ")
+      if (verbose) {
+        message("Testing if worker's PID can be inferred: ", sQuote(test_cmd))
+      }
       res <- system(test_cmd, wait = TRUE, intern = TRUE)
       file.remove(pidfile)
-      
       status <- attr(res, "status")
-      if ((is.null(status) || status == 0L) && any(grepl("42", res))) {
+      if ((is.null(status) || status == 0L) && any(grepl("TRUE", res))) {
+        if (verbose) message("- Possible to infer worker's PID: TRUE")
         rscript_args <- c(rscript_pid_args, rscript_args)
       } else {
+        if (verbose) message("- Possible to infer worker's PID: FALSE")
         pidfile <- NULL
       }
     }
