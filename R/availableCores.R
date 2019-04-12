1b11a34fbb1cc177c8588e4946cd0bbe27654f08
Henrik Bengtsson <hb@aroma-project.org>
2019-04-11 17:29:11 -0700

    Put supportsMulticore() in its own file

--- R/availableCores.R
+++ R/availableCores.R
@@ -203,28 +203,3 @@ availableCores <- function(constraints = NULL, methods = getOption("future.avail
 
   ncores
 } # availableCores()
-
-
-#' Check whether multicore processing is supported or not
-#'
-#' Multicore futures are only supported on systems supporting
-#' multicore processing.  \R supports this on most systems,
-#' except on Microsoft Windows.
-#'
-#' @return TRUE if multicore processing is supported, otherwise FALSE.
-#'
-#' @seealso
-#' To use multicore futures, use \code{\link{plan}(\link{multicore})}.
-#'
-#' @export
-#' @keywords internal
-supportsMulticore <- local({
-  supported <- NA
-  function() {
-    if (is.na(supported)) {
-      ns <- getNamespace("parallel")
-      supported <<- exists("mcparallel", mode = "function", envir = ns, inherits = FALSE)
-    }
-    supported
-  }
-})
