fba7f76f75e2569a782963359a1db35d94cac73e
Henrik Bengtsson <hb@aroma-project.org>
2019-05-29 12:33:34 -0700

    HELP: Clarify return value of nbrOfWorkers() [ci skip]

--- R/nbrOfWorkers.R
+++ R/nbrOfWorkers.R
@@ -4,7 +4,8 @@
 #' If NULL (default), the current evaluator as returned
 #' by \code{\link{plan}()} is used.
 #'
-#' @return A number in [1, Inf].
+#' @return A positive number in \eqn{{1, 2, 3, ...}}.
+#' Note, it may also be \code{+Inf} for certain types of backends.
 #'
 #' @example incl/nbrOfWorkers.R
 #'
