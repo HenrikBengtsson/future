a7f37020bdc7832bf30e2f9593ca953525848cb5
Henrik Bengtsson <hb@aroma-project.org>
2019-06-25 15:10:57 -0700

    Now option 'future.startup.script' falls back to environment variable 'R_FUTURE_STARTUP_SCRIPT'

--- R/Future-class.R
+++ R/Future-class.R
@@ -668,7 +668,7 @@ makeExpression <- local({
       ## covr: skip=7
       ...future.oldOptions <- options(
         ## Prevent .future.R from being source():d when future is attached
-        future.startup.loadScript = FALSE,
+        future.startup.script = FALSE,
         
         ## Assert globals when future is created (or at run time)?
         future.globals.onMissing = .(globals.onMissing),
