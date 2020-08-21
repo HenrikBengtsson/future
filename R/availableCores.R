2de465ba8d8033ece0d4444eb4a5b733c07d772b
Elmar Pruesse <epruesse@users.noreply.github.com>
2020-08-20 21:23:50 -0600

    Update availableCores.R

--- R/availableCores.R
+++ R/availableCores.R
@@ -67,8 +67,6 @@
 #'    \env{LSB_DJOB_NUMPROC}.
 #'    Jobs with multiple (cpu) slots can be submitted on LSF using
 #'    `bsub -n 2 -R "span[hosts=1]" < hello.sh`.
-#'  \Item `"OpenLava"` - 
-#'    This is an alias for LSF.
 #'  \item `"custom"` -
 #'    If option \option{future.availableCores.custom} is set and a function,
 #'    then this function will be called (without arguments) and it's value
@@ -81,7 +79,8 @@
 #'
 #' @section Advanced usage:
 #' It is possible to override the maximum number of cores on the machine
-#' as reported by `availableCores(methods = "system")`.  This can be
+#' as reported by `availableCores(
+"system")`.  This can be
 #' done by first specifying
 #' `options(future.availableCores.methods = "mc.cores")` and
 #' then the number of cores to use, e.g. `options(mc.cores = 8)`.
@@ -95,7 +94,7 @@
 #'
 #' @export
 #' @keywords internal
-availableCores <- function(constraints = NULL, methods = getOption("future.availableCores.methods", c("system", "mc.cores", "_R_CHECK_LIMIT_CORES_", "PBS", "SGE", "Slurm", "fallback", "custom")), na.rm = TRUE, default = c(current = 1L), which = c("min", "max", "all")) {
+availableCores <- function(constraints = NULL, methods = getOption("future.availableCores.methods", c("system", "mc.cores", "_R_CHECK_LIMIT_CORES_", "PBS", "SGE", "Slurm", "LSF", "fallback", "custom")), na.rm = TRUE, default = c(current = 1L), which = c("min", "max", "all")) {
   ## Local functions
   getenv <- function(name) {
     as.integer(trim(Sys.getenv(name, NA_character_)))
@@ -125,8 +124,8 @@ availableCores <- function(constraints = NULL, methods = getOption("future.avail
     } else if (method == "SGE") {
       ## Number of cores assigned by Sun/Oracle Grid Engine (SGE)
       n <- getenv("NSLOTS")
-    } else if (method == "LSF" || method == "OpenLava") {
-      ## Number of slots assigned by LSF or OpenLava
+    } else if (method == "LSF") {
+      ## Number of slots assigned by LSF
       n <- getenv("LSB_DJOB_NUMPROC")
     } else if (method == "mc.cores") {
       ## Number of cores by option defined by 'parallel' package
