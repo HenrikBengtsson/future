588a0075b35bb77b5d33094eaafd7481b2dec989
Henrik Bengtsson <hb@aroma-project.org>
2020-10-17 19:14:21 -0700

    TESTS: Add test for 'split=TRUE' [#424]

--- tests/incl/start,load-only.R
+++ tests/incl/start,load-only.R
@@ -107,3 +107,27 @@ supportedStrategies <- function(cores = 1L, excl = c("multiprocess", "cluster"),
 }
 
 availCores <- min(2L, future::availableCores())
+
+
+## WORKAROUND: capture.output() gained argument 'split' in R 3.3.0
+if (getRversion() >= "3.3.0") {
+  capture.output <- utils::capture.output 
+} else {
+  capture.output <- function(..., split = FALSE) utils::capture.output(...)
+}
+
+recordConditions <- function(expr, ..., parse = TRUE) {
+  conditions <- list()
+  withCallingHandlers(expr, condition = function(c) {
+    attr(c, "received") <- Sys.time()
+    conditions[[length(conditions) + 1L]] <<- c
+  })
+  conditions
+}
+
+recordRelay <- function(...) {
+  stdout <- capture.output(conditions <- recordConditions(...), split = TRUE)
+  if (length(stdout) > 0) stdout <- paste0(stdout, "\n")
+  msgs <- sapply(conditions, FUN = conditionMessage)
+  list(stdout = stdout, msgs = msgs)
+}
