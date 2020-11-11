8166f024229230e963e5b485872256c2cb3ace92
Henrik Bengtsson <hb@aroma-project.org>
2020-11-11 10:38:15 -0800

    TESTS: Now running immediateCondition tests on 'sequential', 'multisession', and 'multicore'

--- tests/immediateCondition.R
+++ tests/immediateCondition.R
@@ -1,6 +1,17 @@
 source("incl/start.R")
-
+plan(sequential)
 options(future.debug = FALSE)
+
+f_try <- function(f, ...) {
+  res <- tryCatch(..., error = identity)
+  if (inherits(res, "error")) {
+    label <- sQuote(f$label)
+    if (length(label) == 0) label <- "<none>"
+    msg <- sprintf("Test with future (%s) of class %s failed assertion: %s", label, sQuote(class(f)[1]), conditionMessage(res))
+    stop(msg, call.=FALSE)
+  }
+  res
+}
 
 message("*** immediateCondition:s ...")
 
@@ -36,7 +47,7 @@ recordMessages <- function(expr, ...) {
 }
 
 ## FIXME: Make sure to set also with cores = 2L /HB 2020-11-10
-strategies <- supportedStrategies(cores = 1L)
+strategies <- supportedStrategies(cores = 2L)
 
 for (ss in seq_along(strategies)) {
   strategy <- strategies[[ss]]
@@ -53,15 +64,17 @@ for (ss in seq_along(strategies)) {
       immediateWarning("IW")
       immediateMessage("IM2")
       42L
-    })
+    }, label = "single-future")
   })
   message("  class: ", paste(sQuote(class(f)), collapse = ", "))
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(f, "UniprocessFuture")) {
-    stopifnot(identical(msgs, c("IM1\n", "IW", "IM2\n")))
-  } else {
-    stopifnot(length(msgs) == 0L)
-  }
+  f_try(f, {
+    if (inherits(f, "UniprocessFuture")) {
+      stopifnot(identical(msgs, c("IM1\n", "IW", "IM2\n")))
+    } else {
+      stopifnot(length(msgs) == 0L)
+    }
+  })
   
   message("- checking if resolved")
   msgs <- recordMessages({
@@ -69,70 +82,79 @@ for (ss in seq_along(strategies)) {
   })
   message("  result: ", r)
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(f, "MultiprocessFuture")) {
-    stopifnot(identical(msgs, c("IM1\n")))
-  } else {
-    stopifnot(length(msgs) == 0L)
-  }
+  f_try(f, {
+    if (inherits(f, "MultiprocessFuture")) {
+      stopifnot(identical(msgs, c("IM1\n", "IW", "IM2\n")))
+    } else {
+      stopifnot(length(msgs) == 0L)
+    }
+  })
 
   message("- resolve w/out collecting results")
   msgs <- recordMessages({
     f <- resolve(f)
   })
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(f, "MultiprocessFuture")) {
-    stopifnot(identical(msgs, c("IW", "IM2\n")))
-  } else {
+  f_try(f, {
     stopifnot(length(msgs) == 0L)
-  }
-
+  })
+  
   message("- resolve w/ collect results")
   msgs <- recordMessages({
     f <- resolve(f, result = TRUE)
   })
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(f, c("UniprocessFuture", "MultiprocessFuture", "CallrFuture", "BatchtoolsFuture"))) {
-    stopifnot(length(msgs) == 0L)
-  } else {
-    stopifnot(identical(msgs, c("IM1\n", "IW", "IM2\n")))
-  }
-
+  f_try(f, {
+    if (inherits(f, c("UniprocessFuture", "MultiprocessFuture", "CallrFuture", "BatchtoolsFuture"))) {
+      stopifnot(length(msgs) == 0L)
+    } else {
+      stopifnot(identical(msgs, c("IM1\n", "IW", "IM2\n")))
+    }
+  })
+  
   message("- getting value")
   msgs <- recordMessages({
     v <- value(f)
   })
   message("  value: ", v)
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(f, c("UniprocessFuture", "MultiprocessFuture"))) {
-    stopifnot(identical(msgs, "M\n"))
-  } else {
-    stopifnot(identical(msgs, c("IM1\n", "IW", "IM2\n", "M\n")))
-  }
+  f_try(f, {
+    if (inherits(f, c("UniprocessFuture", "MultiprocessFuture"))) {
+      stopifnot(identical(msgs, "M\n"))
+    } else {
+      stopifnot(identical(msgs, c("IM1\n", "IW", "IM2\n", "M\n")))
+    }
+  })
   
   message("- getting value again")
   msgs <- recordMessages({
     v <- value(f)
   })
   message("  value: ", v)
-  stopifnot(identical(msgs, "M\n"))
+  f_try(f, {
+    stopifnot(identical(msgs, "M\n"))
+  })
 
   message("* A single future ... DONE")
+
 
   message("* Two futures ... ")
 
   message("- list of two futures")
   fs <- list()
   msgs <- recordMessages({
-    fs[[1]] <- future({ immediateMessage("IM1"); Sys.sleep(0.1); message("M1"); immediateWarning("IW1"); 1L })
-    fs[[2]] <- future({ immediateMessage("IM2"); Sys.sleep(0.1); message("M2"); immediateWarning("IW2"); 2L })
+    fs[[1]] <- future({ immediateMessage("IM1"); Sys.sleep(0.1); message("M1"); immediateWarning("IW1"); 1L }, label = "future-1")
+    fs[[2]] <- future({ immediateMessage("IM2"); Sys.sleep(0.1); message("M2"); immediateWarning("IW2"); 2L }, label = "future-2")
   })
   message("  class: ", paste(sQuote(class(fs[[1]])), collapse = ", "))
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(fs[[1]], "UniprocessFuture")) {
-    stopifnot(identical(msgs, c("IM1\n", "IW1", "IM2\n", "IW2")))
-  } else {
-    stopifnot(length(msgs) == 0L)
-  }
+  f_try(fs[[1]], {
+    if (inherits(fs[[1]], "UniprocessFuture")) {
+      stopifnot(identical(msgs, c("IM1\n", "IW1", "IM2\n", "IW2")))
+    } else {
+      stopifnot(length(msgs) == 0L)
+    }
+  })
 
   message("- check if resolved")
   msgs <- recordMessages({
@@ -140,33 +162,40 @@ for (ss in seq_along(strategies)) {
   })
   message("  result: ", paste(rs, collapse = ", "))
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(f, "MultiprocessFuture")) {
-    stopifnot(identical(msgs, c("IM1\n", "IM2\n")))
-  } else {
-    stopifnot(length(msgs) == 0L)
-  }  
+  f_try(f, {
+    if (inherits(f, "MultiprocessFuture")) {
+      stopifnot(all(msgs %in% c("IM1\n", "IM2\n", "IW1", "IW2")))
+      if (inherits(f, "MulticoreFuture")) {
+        stopifnot(identical(msgs, c("IM1\n", "IM2\n", "IW1", "IW2")))
+      } else if (inherits(f, "MultisessionFuture")) {
+        stopifnot(identical(msgs, c("IM1\n", "IW1", "IM2\n", "IW2")))
+      }
+    } else {
+      stopifnot(length(msgs) == 0L)
+    }
+  })
 
   message("- resolve w/out collecting results")
   msgs <- recordMessages({
     fs <- resolve(fs)
   })
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(f, "MultiprocessFuture")) {
-    stopifnot(identical(msgs, c("IW1", "IW2")))
-  } else {
+  f_try(f, {
     stopifnot(length(msgs) == 0L)
-  }  
+  })
 
   message("- resolve w/ collect results")
   msgs <- recordMessages({
     fs <- resolve(fs, result = TRUE)
   })
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  if (inherits(fs[[1]], c("UniprocessFuture", "MultiprocessFuture", "CallrFuture", "BatchtoolsFuture"))) {
-    stopifnot(length(msgs) == 0L)
-  } else {
-    stopifnot(identical(msgs, c("IM1\n", "IW1", "IM2\n", "IW2")))
-  }
+  f_try(f, {
+    if (inherits(fs[[1]], c("UniprocessFuture", "MultiprocessFuture", "CallrFuture", "BatchtoolsFuture"))) {
+      stopifnot(length(msgs) == 0L)
+    } else {
+      stopifnot(identical(msgs, c("IM1\n", "IW1", "IM2\n", "IW2")))
+    }
+  })
 
   message("- getting value")
   msgs <- recordMessages({
@@ -174,7 +203,9 @@ for (ss in seq_along(strategies)) {
   })
   message("  values: ", paste(vs, collapse = ", "))
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  stopifnot(identical(msgs, c("M1\n", "M2\n")))
+  f_try(f, {
+    stopifnot(identical(msgs, c("M1\n", "M2\n")))
+  })
   
   message("- getting value again")
   msgs <- recordMessages({
@@ -182,7 +213,9 @@ for (ss in seq_along(strategies)) {
   })
   message("  values: ", paste(vs, collapse = ", "))
   message(sprintf("  msgs [n=%d]: %s", length(msgs), paste(sQuote(msgs), collapse = ", ")))
-  stopifnot(identical(msgs, c("M1\n", "M2\n")))
+  f_try(f, {
+    stopifnot(identical(msgs, c("M1\n", "M2\n")))
+  })
 
   message("- getting values one by one")
   msgs <- recordMessages({
@@ -192,7 +225,9 @@ for (ss in seq_along(strategies)) {
     )
   })
   message("  values: ", paste(vs, collapse = ", "))
-  stopifnot(identical(msgs, c("M1\n", "M2\n")))
+  f_try(f, {
+    stopifnot(identical(msgs, c("M1\n", "M2\n")))
+  })
 
   message("* Two futures ... DONE")
 
