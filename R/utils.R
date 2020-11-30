b690bcf65d487ef874d42780f219e5fb8683b7ce
Henrik Bengtsson <hb@aroma-project.org>
2020-11-29 19:43:57 -0800

    CLEANUP: Drop unused code now part of 'parallelly'

--- R/utils.R
+++ R/utils.R
@@ -221,36 +221,6 @@ getOption <- local({
 }) ## getOption()
 
 
-detectCores <- local({
-  res <- NULL
-  function() {
-    if (is.null(res)) {
-      ## Get number of system cores from option, system environment,
-      ## and finally detectCores().  This also designed such that
-      ## it is indeed possible to return NA_integer_.
-      value <- getOption("future.availableCores.system")
-      if (!is.null(value)) {
-        value <- as.integer(value)
-        return(value)
-      }
-      
-      value <- parallel::detectCores()
-      
-      ## If unknown, set default to 1L
-      if (is.na(value)) value <- 1L
-      value <- as.integer(value)
-      
-      ## Assert positive integer
-      stop_if_not(length(value) == 1L, is.numeric(value),
-                is.finite(value), value >= 1L)
-
-      res <<- value
-    }
-    res
-  }
-})
-
-
 ## We are currently importing the following non-exported functions:
 ## * cluster futures:
 ##   - parallel:::defaultCluster()  ## non-critical / not really needed /
