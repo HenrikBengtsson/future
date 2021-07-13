f8ea4b6e181d26c8cfac8866b4873c683daa7cd8
Henrik Bengtsson <hb@aroma-project.org>
2021-07-13 14:19:29 +0200

    TESTS: Make tests for non-exportable connections more robust

--- tests/non-exportable,connections.R
+++ tests/non-exportable,connections.R
@@ -2,19 +2,28 @@ source("incl/start.R")
 
 message("*** Non-exportable globals ...")
 
+options(future.debug = FALSE)
 options(future.globals.onReference = "warning")
-plan(multisession, workers = 2L)
+plan(cluster, workers = 1L)
 
 
 message("* R connections ...")
 
+## Create a dummy, global connection with a high enough connection index
+## such that it won't conflict with an existing connection index on the
+## worker. We use sink():s to "pad" with enough connections
 tmp_file <- tempfile()
-con <- file(tmp_file, open = "wb")
+utils::capture.output(utils::capture.output({
+  con <- file(tmp_file, open = "wb")
+}))
 cat("hello\n", file = con)
 flush(con)
 bfr <- readLines(con = tmp_file)
 print(bfr)
 stopifnot(bfr == "hello")
+message(sprintf("Connection index of dummy 'con' connection: %d\n", as.integer(con)))
+stopifnot(as.integer(con) >= 6)
+
 
 message("- Run-time error")
 
@@ -26,11 +35,14 @@ print(res)
 stopifnot(inherits(res, "FutureWarning"),
           grepl("non-exportable reference", conditionMessage(res)))
 
-## ISSUE: When using stdout = TRUE/FALSE below, a connection will be opened
-## on the worker (due to sink:ing), which then will be hijacked by the
-## global 'con' connection in the below expression.  Because of this, we
-## have to test with stdout = NA.
-f <- future(cat("world\n", file = con), stdout = NA)
+f <- future({
+  max <- max(getAllConnections(), na.rm = TRUE)
+  cat(sprintf("Max connection index on worker: %d\n", max))
+  if (as.integer(con) <= max) {
+    warning(sprintf("TEST ERROR: Index of global 'con' is the same of an existing connection of the worker: %d in [0,%d]", as.integer(con), max))
+  }
+  cat("world\n", file = con)
+})
 res <- tryCatch({
   v <- value(f)
 }, error = identity)
