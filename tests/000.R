434b82f8b3765c04663e2ab5f80186a220659690
Henrik Bengtsson <hb@aroma-project.org>
2021-08-11 16:38:13 +0200

    TROUBLESHOOT: FORK on macOS - A14

--- tests/000.R
+++ tests/000.R
@@ -4,10 +4,10 @@ pid <- Sys.getpid()
 message("Main PID: ", pid)
 
 if (exists("mcparallel", mode="function", envir=getNamespace("parallel"))) {
-  cl <- makeCluster(1L, type = "FORK", timeout = 60)
+  cl <- makeCluster(1L, type = "FORK")
 
-  ## Force R worker to quit
-  res <- tryCatch(x <- clusterEvalQ(cl, quit(save = "no")), error = identity)
+  ## Emulate R worker crash
+  res <- tryCatch(clusterEvalQ(cl, quit(save = "no")), error = identity)
   stopifnot(inherits(res, "error"))
 }
 
