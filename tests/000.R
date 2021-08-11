be67ac5d64e607230d6e70314433f52ee9809da7
Henrik Bengtsson <hb@aroma-project.org>
2021-08-11 16:12:51 +0200

    TROUBLESHOOT: FORK on macOS - A8

--- tests/000.R
+++ tests/000.R
@@ -7,30 +7,36 @@ if (exists("mcparallel", mode="function", envir=getNamespace("parallel"))) {
   cl <- makeCluster(1L, type = "FORK", timeout = 60)
   print(cl)
 
-  message("*** cluster() - crashed worker ...")
-
   x <- clusterEvalQ(cl, 42L)
   stopifnot(x == 42L)
+  pid2 <- Sys.getpid()
+  message("Main PID: ", pid2)
+  message("Main PID (original): ", pid)
+  stopifnot(pid2 == pid)
   
   ## Force R worker to quit
   res <- tryCatch(x <- clusterEvalQ(cl, quit(save = "no")), error = identity)
   print(res)
   stopifnot(inherits(res, "error"))
+  pid2 <- Sys.getpid()
+  message("Main PID: ", pid2)
+  message("Main PID (original): ", pid)
+  stopifnot(pid2 == pid)
 
   ## Cleanup
   print(cl)
   ## FIXME: Why doesn't this work here? It causes the below future to stall.
   # stopCluster(cl)
+  pid2 <- Sys.getpid()
+  message("Main PID: ", pid2)
+  message("Main PID (original): ", pid)
+  stopifnot(pid2 == pid)
 
   ## Verify that the reset worked
   cl <- makeCluster(1L, type = "FORK", timeout = 60)
   print(cl)
   x <- clusterEvalQ(cl, 43L)
   stopifnot(x == 43L)
-  
-  message("*** cluster() - crashed worker ... DONE")
-
-  ## Sanity checks
   pid2 <- Sys.getpid()
   message("Main PID: ", pid2)
   message("Main PID (original): ", pid)
