9495713f7293bc417a0a8836bc11ba6c3757ac25
Henrik Bengtsson <hb@aroma-project.org>
2021-08-26 09:49:53 +0200

    CLEANUP: No longer importing 'graphics' and 'grDevices'

--- tests/mandelbrot.R
+++ tests/mandelbrot.R
@@ -3,11 +3,7 @@ source("incl/start.R")
 message("mandelbrot() ...")
 
 counts <- mandelbrot(xmid = -0.75, ymid = 0.0, side = 3.0, resolution = 100L)
-img <- as.raster(counts)
-if (getRversion() >= "3.2.0") {
-  plot(img)
-  plot(counts)
-}
+if (getRversion() >= "3.2.0") plot(counts)
 
 message("mandelbrot() ... DONE")
 
