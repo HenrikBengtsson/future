fbc176675ffa704a5014ae6af2c4b6620980e468
Henrik Bengtsson <hb@aroma-project.org>
2021-09-25 22:24:55 -0700

    Typo: Should have said value() but said values()

--- R/999.DEPRECATED.R
+++ R/999.DEPRECATED.R
@@ -3,6 +3,6 @@
 ## - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #' @export
 values <- function(...) {
-  .Deprecated(msg = "values() is deprecated in future (>= 1.20.0). Instead, use values().", package = .packageName)
+  .Deprecated(msg = "values() is deprecated in future (>= 1.20.0). Instead, use value().", package = .packageName)
   value(...)
 }
