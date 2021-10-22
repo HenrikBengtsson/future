be5a5537ca633e173f003f4711d0ef7b5864faad
Henrik Bengtsson <hb@aroma-project.org>
2021-10-21 19:28:35 -0700

    TESTS: Avoid making test script file UTF-8 encoded by generating UTF-8 strings at run-time [#473]

--- tests/adhoc_native_to_utf8.R
+++ tests/adhoc_native_to_utf8.R
@@ -4,12 +4,21 @@ adhoc_native_to_utf8 <- future:::adhoc_native_to_utf8
 
 message("*** adhoc_native_to_utf8( ) ...")
 
+## WORKAROUND: In order to avoid this test script itself to use UTF-8
+## encoding, we generate such UTF-8 strings at run-time instead from
+## escaped UTF-8 strings.
 strings <- c(
-       "[x] hello" = "[x] hello",
-    "\u2713 hello" = "<U+2713> hello",
-  "'\u2713 hello'" = "'<U+2713> hello'",
-  '"\u2713 hello"' = '"<U+2713> hello"'
+        "[x] hello" = "[x] hello",
+    "\\u2713 hello" = "<U+2713> hello",
+  "'\\u2713 hello'" = "'<U+2713> hello'",
+  '"\\u2713 hello"' = '"<U+2713> hello"'
 )
+for (kk in seq_along(strings)) {
+  name <- names(strings)[kk]
+  code <- paste('"', gsub('"', '\\"', name, fixed = TRUE), '"', sep = "")
+  name <- eval(parse(text = code))
+  names(strings)[kk] <- name
+}
 
 for (kk in seq_along(strings)) {
   message(sprintf("Test case #%d:", kk))
