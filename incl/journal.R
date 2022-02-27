b39ca26b50608e5fa51dd0997f38f10c9996b25d
Henrik Bengtsson <hb@aroma-project.org>
2022-02-26 21:01:58 -0800

    Avoid exporting journal() for now; it'll be under heavy development for quite some time

---
+++ incl/journal.R
@@ -0,0 +1,12 @@
+library(future)
+journal <- future:::journal
+plan(multisession, workers = 2L)
+
+t_start <- Sys.time()
+fs <- lapply(1:3, FUN = function(x) future({ Sys.sleep(x); sqrt(x) }))
+vs <- value(fs)
+js <- lapply(fs, FUN = journal, baseline = t_start)
+print(js)
+
+## Stop parallel workers
+plan(sequential)
