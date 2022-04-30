fecb5a18be7fb46d543e6ea119f36b0c51819f1b
Henrik Bengtsson <hb@aroma-project.org>
2022-04-29 22:48:17 -0700

    Let 'future.hidden.features' default to TRUE for now

--- R/Future-class.R
+++ R/Future-class.R
@@ -105,13 +105,13 @@ Future <- function(expr = NULL, envir = parent.frame(), substitute = TRUE, stdou
   }
 
   stop_if_not(is.logical(stdout), length(stdout) == 1L)
-  if (is.na(stdout) && !getOption("future.hidden.features", FALSE)) {
+  if (is.na(stdout) && !getOption("future.hidden.features", TRUE)) {
     stop("Argument 'stdout' should be either TRUE or FALSE: NA")
   }
   
   if (!is.null(conditions)) {
     stop_if_not(is.character(conditions), !anyNA(conditions))
-  } else if (!getOption("future.hidden.features", FALSE)) {
+  } else if (!getOption("future.hidden.features", TRUE)) {
     stop("Argument 'conditions' should be a character vector: NULL")
   }
   
