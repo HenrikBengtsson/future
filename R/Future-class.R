778fc6de58dc6977e466a9d8ee41bd490ccac1fc
Henrik Bengtsson <hb@aroma-project.org>
2022-04-29 22:22:04 -0700

    HELP: Document argument drop = TRUE (fix #612)

--- R/Future-class.R
+++ R/Future-class.R
@@ -19,6 +19,10 @@
 #' and re-outputted when `value()` is called.
 #' If FALSE, any output is silenced (by sinking it to the null device as
 #' it is outputted).
+#' Using `stdout = structure(TRUE, drop = TRUE)` causes the captured
+#' standard output to be dropped from the future object as soon as it has
+#' been relayed by, for instance, `value(f)`. This can help decrease the
+#' overall memory consumed by captured output across futures.
 #' If NA (not recommended), output is _not_ intercepted.
 #' 
 #' @param conditions A character string of conditions classes to be captured
@@ -27,6 +31,10 @@
 #' Attribute `exclude` can be used to ignore specific classes, e.g.
 #' `conditions = structure("condition", exclude = "message")` will capture
 #' all `condition` classes except those that inherits from the `message` class.
+#' Using `conditions = structure(..., drop = TRUE)` causes the captured
+#' conditions to be dropped from the future object as soon as it has
+#' been relayed by, for instance, `value(f)`. This can help decrease the
+#' overall memory consumed by captured conditions across futures.
 #' Errors are always relayed.
 #' 
 #' @param globals (optional) a logical, a character vector, or a named list
