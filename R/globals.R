903b016c2bc902a01290f66f469e2214a2d9e249
Henrik Bengtsson <hb@aroma-project.org>
2022-05-26 11:38:16 -0700

    For now, make hidden option 'future.globals.globalsOf.locals' default to TRUE for backward compatibility

--- R/globals.R
+++ R/globals.R
@@ -30,7 +30,7 @@
 #' @export
 #'
 #' @keywords internal
-getGlobalsAndPackages <- function(expr, envir = parent.frame(), tweak = tweakExpression, globals = TRUE, locals = getOption("future.globals.globalsOf.locals", FALSE), resolve = getOption("future.globals.resolve", NULL), persistent = FALSE, maxSize = getOption("future.globals.maxSize", 500 * 1024 ^ 2), ...) {
+getGlobalsAndPackages <- function(expr, envir = parent.frame(), tweak = tweakExpression, globals = TRUE, locals = getOption("future.globals.globalsOf.locals", TRUE), resolve = getOption("future.globals.resolve", NULL), persistent = FALSE, maxSize = getOption("future.globals.maxSize", 500 * 1024 ^ 2), ...) {
   if (is.null(resolve)) {
     resolve <- FALSE
   } else {
