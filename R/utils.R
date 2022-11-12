d1876d049fcf7efb6c0873267aa72a859f336bf9
Henrik Bengtsson <hb@aroma-project.org>
2022-11-12 10:24:12 -0800

    CLEANUP: Internal getOption() speedup tweak is only needed in R (< 4.0.0) (fix #655) [ci skip]

--- R/utils.R
+++ R/utils.R
@@ -274,16 +274,18 @@ requirePackages <- local(function(pkgs) {
 }) ## requirePackages()
 
 
-## When 'default' is specified, this is 30x faster than
-## base::getOption().  The difference is that here we use
-## use names(.Options) whereas in 'base' names(options())
-## is used.
-getOption <- local({
-  go <- base::getOption
-  function(x, default = NULL) {
-    if (missing(default) || match(x, table = names(.Options), nomatch = 0L) > 0L) go(x) else default
-  }
-}) ## getOption()
+if (getRversion() < "4.0.0") {
+  ## When 'default' is specified, this is 30x faster than
+  ## base::getOption().  The difference is that here we use
+  ## use names(.Options) whereas in 'base' names(options())
+  ## is used.
+  getOption <- local({
+    go <- base::getOption
+    function(x, default = NULL) {
+      if (missing(default) || match(x, table = names(.Options), nomatch = 0L) > 0L) go(x) else default
+    }
+  })
+}
 
 
 ## We are currently importing the following non-exported functions:
